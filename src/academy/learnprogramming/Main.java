9c7dc21b6d981876198fa1f8dbbb57b5df8c6d49
jinook929 <jinook929@gmail.com>
2019-11-21 23:02:12 -0500

    Initial commit

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,82 @@
+package academy.learnprogramming;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// Create a class for int type
+class IntClass {
+    private int myValue;
+
+    public IntClass(int myValue) {
+        this.myValue = myValue;
+    }
+
+    public int getMyValue() {
+        return myValue;
+    }
+
+    public void setMyValue(int myValue) {
+        this.myValue = myValue;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Create arrays for int and String
+        String[] strArray = new String[10];
+        System.out.println(Arrays.toString(strArray));
+        int[] intArray = new int[10];
+        System.out.println(Arrays.toString(intArray));
+
+        // Create ArrayList for String
+        ArrayList<String> strArrayList = new ArrayList<>();
+        strArrayList.add("Tim");
+        strArrayList.add("Tom");
+        strArrayList.add("John");
+        System.out.println(strArrayList.toString());
+//        for(int i = 0; i < strArrayList.size(); i++) {
+//            System.out.println(strArrayList.get(i));
+//        }
+
+        // Create ArrayList for int
+        ArrayList<IntClass> intClassArrayList = new ArrayList<>();
+        intClassArrayList.add(new IntClass(54));
+        intClassArrayList.add(new IntClass(55));
+//        System.out.println(intClassArrayList.toString());
+        for(int i = 0; i < intClassArrayList.size(); i++) {
+            System.out.println(intClassArrayList.get(i).getMyValue());
+        }
+
+        ArrayList<Integer> intArrayList = new ArrayList<>();
+        for(int i = 0; i < 10; i++) {
+            intArrayList.add(i);
+        }
+        System.out.println(intArrayList.toString());
+        for(int i = 0; i < 10; i++) {
+            System.out.println(i + " --> " + intArrayList.get(i).intValue());
+        }
+
+        Integer integer = new Integer(77);
+        System.out.println(integer);
+        Integer integer1 = 78;
+        System.out.println(integer1);
+        Integer integer2 = Integer.valueOf(79);
+        System.out.println(integer2);
+
+        int myInt = integer1.intValue();
+        System.out.println(myInt);
+
+        Double doubleValue = new Double(11.11);
+        System.out.println(doubleValue);
+        Double doubleValue1 = 22.22;
+        System.out.println(doubleValue1);
+
+        ArrayList<Double> myDoubleValues= new ArrayList<>();
+        for(double dbl = 0.0; dbl <= 10.0; dbl += 0.5) {
+            myDoubleValues.add(dbl);
+        }
+        System.out.println(myDoubleValues.toString());
+    }
+}
