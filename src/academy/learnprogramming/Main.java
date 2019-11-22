dbf17f431e0157bcf8241f23f9ec8c0177ed006e
jinook929 <jinook929@gmail.com>
2019-11-21 23:26:18 -0500

    Try some more codes

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -30,7 +30,7 @@ public class Main {
         int[] intArray = new int[10];
         System.out.println(Arrays.toString(intArray));
 
-        // Create ArrayList for String
+        // Create ArrayList for String(= class)
         ArrayList<String> strArrayList = new ArrayList<>();
         strArrayList.add("Tim");
         strArrayList.add("Tom");
@@ -40,21 +40,22 @@ public class Main {
 //            System.out.println(strArrayList.get(i));
 //        }
 
-        // Create ArrayList for int
+        // Create ArrayList for int(=primitive type) => autoboxing/unboxing
         ArrayList<IntClass> intClassArrayList = new ArrayList<>();
         intClassArrayList.add(new IntClass(54));
         intClassArrayList.add(new IntClass(55));
+        intClassArrayList.add(new IntClass(56));
 //        System.out.println(intClassArrayList.toString());
         for(int i = 0; i < intClassArrayList.size(); i++) {
             System.out.println(intClassArrayList.get(i).getMyValue());
         }
 
         ArrayList<Integer> intArrayList = new ArrayList<>();
-        for(int i = 0; i < 10; i++) {
-            intArrayList.add(i);
+        for(int i = 0; i < 5; i++) {
+            intArrayList.add(Integer.valueOf(i));
         }
         System.out.println(intArrayList.toString());
-        for(int i = 0; i < 10; i++) {
+        for(int i = 0; i < intArrayList.size(); i++) {
             System.out.println(i + " --> " + intArrayList.get(i).intValue());
         }
 
@@ -67,14 +68,17 @@ public class Main {
 
         int myInt = integer1.intValue();
         System.out.println(myInt);
+        myInt = integer2;
+        System.out.println(myInt);
 
+        // Create ArrayList for double(=primitive type) => autoboxing
         Double doubleValue = new Double(11.11);
         System.out.println(doubleValue);
         Double doubleValue1 = 22.22;
         System.out.println(doubleValue1);
 
         ArrayList<Double> myDoubleValues= new ArrayList<>();
-        for(double dbl = 0.0; dbl <= 10.0; dbl += 0.5) {
+        for(double dbl = 0.0; dbl <= 5.0; dbl += 0.5) {
             myDoubleValues.add(dbl);
         }
         System.out.println(myDoubleValues.toString());
